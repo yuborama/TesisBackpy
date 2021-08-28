69b06f29e6d27ef3f1644465c63a3ad8dcd9fbe5
Yuborama <davidjohan2@hotmail.com>
2021-08-27 19:10:45 -0600

    Fix date excel and multiple files

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
 # importaciones
 print("hola1")
 from flask import Flask, jsonify, request,send_file
-print("hola2")
 from flask_cors import CORS
-print("hola3")
 import pandas as pd
-print("hola4")
 from datetime import datetime
-print("hola5")
 from io import BytesIO
-print("hola6")
 import re
 from xlsxwriter import Workbook
 # import pipreqs
-print("hola7")
 # print(pipreqs --print ./);
 # from werkzeug.utils import send_file
 
 app = Flask(__name__)
 
 CORS(app)
+def convertdatestr(x):
+    y = pd.to_datetime(str(x),format='%Y-%m-%d %H:%M:%S',errors='raise')
+    return y.strftime('%Y-%d-%m %H:%M:%S')
 
+converterdatastr = lambda x : pd.to_datetime(str(x),format='%Y-%m-%d %H:%M:%S',errors='ignore') 
+converter = lambda x : (pd.to_datetime(x,format='%m/%d/%Y %I:%M:%S %p',errors='ignore')) if re.findall('[AMPM]$',str(x)) else ((pd.to_datetime(x,format='%Y-%d-%m %H:%M:%S',errors='ignore')) if re.findall('-',str(x)) else (pd.to_datetime(str(x),errors='ignore')))
 # funcion devoler dataframe segun colunmas
 def df_columns(file,columns):
     Datos = pd.read_excel(file, parse_dates=False,dtype={'Hasta*':'str','Desde*':'str'})    
@@ -30,7 +29,6 @@ def df_columns(file,columns):
 
 def calculate_velocity(df):
     print("hola8")
-    converter = lambda x : (pd.to_datetime(x,format='%m/%d/%Y %I:%M:%S %p',errors='ignore')) if re.findall('[AMPM]$',str(x)) else ((pd.to_datetime(x,format='%Y-%d-%m %H:%M:%S',errors='ignore')) if re.findall('-',str(x)) else (pd.to_datetime(str(x),errors='ignore')))
     print("hola9")
     df['diferencia'] = df['Hasta*'].apply(converter)-df['Desde*'].apply(converter)
     print("hola10")
@@ -51,9 +49,13 @@ def mergedfs(df,df2):
 def export_df(df):
     df['Desde*'] = pd.to_datetime('today').normalize()
     df['Hasta*'] = (pd.to_datetime('today').normalize()+df['diferencia'])
+    df['Desde*'] = df['Desde*'].apply(convertdatestr)
+    df['Hasta*'] = df['Hasta*'].apply(convertdatestr)
     df['MD from (ft)'] = 0
     df['P/N*'] = 'P'
     df = df.rename(columns={'perforacion':'MD to (ft)'})
+    # filta los que tienen valor
+    # df = df.loc[df['MD to (ft)'] != 0]
     return df[['Desde*','Hasta*','Subcódigo*','P/N*','MD from (ft)','MD to (ft)']]
 
 def convertStrToDate(date):
@@ -89,16 +91,17 @@ def homepost():
 @app.route('/file', methods=['POST'])
 def recivedfile():
     if request.files:
-        file = request.files["filename"]
-        print('file')
-        print(file)
-        if file:
-            archivo = pd.read_excel(file)
-            fechas = archivo[['Desde*','Hasta*','Subcódigo*','P/N*']].copy()
-            fechas['Tiempo'] = date(fechas['Hasta*'])-date(fechas['Desde*'])
-            fechas['minutes']= calculateminutes(fechas['Tiempo'])
-            data = fechas.loc[fechas['P/N*']=='N'].groupby('Subcódigo*').sum().reset_index().to_dict('records')
-            return jsonify({'data':data})
+        files = request.files.getlist('filename')
+        if files:
+            datalist = []
+            for file in files:
+                fechas = df_columns(file,['Desde*','Hasta*','Subcódigo*','P/N*','MD from (ft)','MD to (ft)'])
+                fechas['diferencia'] = fechas['Hasta*'].apply(converter)-fechas['Desde*'].apply(converter)
+                fechas['hours']= fechas['diferencia'].dt.total_seconds()/3600
+                data = fechas.loc[fechas['P/N*']=='P',['Subcódigo*','hours']].groupby('Subcódigo*').sum().reset_index().to_dict('records')
+                dicty ={'File':file.filename,"datos":data}
+                datalist.append(dicty)
+            return jsonify({'data':datalist})
         return jsonify({'message':'please all data'})
     return jsonify({'message':'upload files'})
 
@@ -115,13 +118,15 @@ def donwload():
                 # print("file: ",file)
                 df_2 = df_columns(file,['Desde*','Hasta*','Subcódigo*','P/N*','MD from (ft)','MD to (ft)'])
                 df2 = calculate_velocity(df_2)
+                print('miradme', df2.dtypes)
                 df_1 = mergedfs(df_1,df2)
             else:
                 print('entro else')
                 df_1=df_columns(file,['Desde*','Hasta*','Subcódigo*','P/N*','MD from (ft)','MD to (ft)'])
                 df_1 = calculate_velocity(df_1)
-        print('df\n',df_1.dtypes)
         df_1 = export_df(df_1)
+        print('df\n',df_1.dtypes)
+        print(df_1) 
         output = BytesIO()
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
         df_1.to_excel(writer, startrow = 0, merge_cells = False, sheet_name = "Sheet_1", index=False)
